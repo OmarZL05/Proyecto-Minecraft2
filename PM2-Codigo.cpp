dfb16f930cc50b22a5453e222b31541c22fd0b88
Your Name <OmarZL05>
2024-02-27 16:07:52 -0400

    Actualizacion

--- PM2-Codigo.cpp
+++ PM2-Codigo.cpp
@@ -12,7 +12,7 @@ struct jugador
 {
     string nickname;
     string nombre;
-    string experienza;
+    string experiencia;
     slot mochila[3];
 };
 
@@ -25,140 +25,144 @@ struct alianza
     jugador jugadores[8];
 };
 
-void registrarJugador(jugador &jug) {
+void registrarJugador(jugador &jug)
+{
     int op;
     cout << "Indique el nombre: ";
     getline(cin >> ws, jug.nombre);
     cout << "Indique el nickname en minecraft: ";
     cin >> jug.nickname;
-    cout << "Elija su nivel de experiencia [1 = Pro] [2 = Noob]: ";
-    do {
+    cout << "Elija su nivel de experiencia [1: pro] [2: noob]" << endl;
+    do
+    {
         cin >> op;
-        switch (op) {
-            case 1:
-                jug.experienza = "pro";
-                break;
-            case 2:
-                jug.experienza = "noob";
-                break;
-            default:
-                cout << "Escoja una de las opciones indicadas.";
+        switch (op)
+        {
+        case 1:
+            jug.experiencia = "pro";
+            break;
+        case 2:
+            jug.experiencia = "noob";
+            break;
+        default:
+            cout << "Escoja una de las opciones indicadas.";
         }
-    } while(op < 1 || op > 2);
-
+    } while (op < 1 || op > 2);
+        cout << "Puedes llevar 3 items en tu mochila." << endl;
+    for (int i = 0; i < 3; i++)
+    {
+        cout << "Tienes " << 3-i << " slots libres." << endl;
+        cout << "Indique el item. [pico] [pala] [hacha]" << endl;
+        cin >> jug.mochila[i].nombre;
+        cout << "Indique el material del item. [madera] [piedra] [oro] [hierro] [diamante]" << endl;
+        cin >> jug.mochila[i].tipo;
+    }
 }
 
-void registrarJugadores(alianza &ali) {
+void registrarJugadores(alianza &ali)
+{
     char op = 's';
-    int i=0;
+    int i = 0;
     cout << "Registre a continuacion los integrantes de su equipo" << endl;
     cout << "Nota: El maximo de jugadores es 8" << endl;
-    while(op == 's' && i<8) {
-        cout << "Ingrese los datos del jugador " << i+1 << endl;
+    while (op == 's' && i < 8)
+    {
+        cout << "Ingrese los datos del jugador " << i + 1 << endl;
         registrarJugador(ali.jugadores[i]);
-        cout << "¿Desea registrar otro integrante? [s = Si] [s <> No]: ";
-        cin >> op;
+        if (i < 7)
+        {
+            cout << "¿Desea registrar otro integrante? [s:si] [otro:no]: ";
+            cin >> op;
+        }
         i++;
     }
-
 }
 
-void registrarAlianza(alianza &ali) {
+void registrarAlianza(alianza &ali)
+{
     cout << "Indique el nombre de la alianza: ";
     cin >> ali.nombre;
+    ali.esmeraldas = 0;
+    ali.puntosAFavor = 0;
+    ali.puntosEnContra = 0;
     registrarJugadores(ali);
-
 }
 
-void registrarAlianzas(alianza alianzas[]) {
-    for(int i=0;i<4;i++) {
-        cout << "\nBienvenido, estás a punto de registrar la alianza N." << i+1 << endl;
-        registrarAlianza(alianzas[i]);
-    }
-}
-
-int equipoMinar(string equipo) {
-    cout << "Indica la cantidad de menas minadas equipo " << equipo << ".";
+int equipoMinar(string equipo)
+{
+    cout << "\nIndica la cantidad de menas minadas, alianza " << equipo << "." << endl;
     int cantDiamantes, cantOro, cantHierro;
+   
+    cantDiamantes = 0;
+    cantOro = 0;
+    cantHierro = 0;
+
     cout << "Menas de diamantes minadas: ";
     cin >> cantDiamantes;
     cout << "Menas de hierro minadas: ";
     cin >> cantHierro;
     cout << "Menas de oro minadas: ";
-    cin >> cantHierro;
+    cin >> cantOro;
 
-    return cantDiamantes*25 + cantOro*10 + cantHierro;
-
+    return (cantDiamantes*25) + (cantOro * 10) + cantHierro;
 }
 
-void iniciarRonda(alianza &equipo1, alianza &equipo2, bool rondaFinal) {
+void iniciarRonda(alianza &equipo1, alianza &equipo2)
+{
     int ptsEquipo1, ptsEquipo2;
-    bool op;
+    char op;
 
     ptsEquipo1 = equipoMinar(equipo1.nombre);
     ptsEquipo2 = equipoMinar(equipo2.nombre);
 
     equipo1.puntosAFavor += ptsEquipo1;
     equipo1.puntosEnContra += ptsEquipo2;
-    
+
     equipo2.puntosAFavor += ptsEquipo2;
     equipo2.puntosEnContra += ptsEquipo1;
 
-    if(ptsEquipo1 > ptsEquipo2) {
+    if (ptsEquipo1 > ptsEquipo2)
+    {
         // equipo1 gano
         equipo1.esmeraldas += 3;
-        
     }
 
-    if(ptsEquipo1 < ptsEquipo2) {
+    if (ptsEquipo1 < ptsEquipo2)
+    {
         // equipo2 gano
         equipo2.esmeraldas += 3;
     }
- 
-    if(ptsEquipo1 == ptsEquipo2) {
-        // empataron
-        if(rondaFinal == true) {
-            
-        } else {
-            equipo1.esmeraldas++;
-            equipo2.esmeraldas++;
-        }
+
+    if (ptsEquipo1 == ptsEquipo2)
+    {
+        equipo1.esmeraldas++;
+        equipo2.esmeraldas++;
     }
-    
 }
 
-void clasificacion() {
-    /* 
-         
-        [i, j]
-        [1, 2]
-        [1, 3]
-        [1, 4]
-        [2, 3]
-        [2, 4]
-        [3, 4]
-        
-        [ [1,2], [1,3], [1,4], [2,3], [2,4], [3,4] ]
-        iniciarRonda(alianzas[1], alianzas[2]);
-        iniciarRonda(alianzas[1], alianzas[3]);
-        iniciarRonda(alianzas[1], alianzas[4]);
-        iniciarRonda(alianzas[2], alianzas[3]);
-        iniciarRonda(alianzas[2], alianzas[4]);
-        iniciarRonda(alianzas[3], alianzas[4]);
-        while(j<4) {
-            if(r1 == r2) {
-                
-            }
-            j++;
-        }
-
-    */
-}
-
-int main() {
+int main()
+{
     // Variables
     alianza alianzas[4];
 
-    registrarAlianzas(alianzas);
+    for (int i = 0; i < 4; i++)
+    {
+        cout << "\nBienvenido, estás a punto de registrar la alianza N." << i + 1 << endl;
+        registrarAlianza(alianzas[i]);
+    }
 
+    iniciarRonda(alianzas[0], alianzas[1]);
+    iniciarRonda(alianzas[0], alianzas[2]);
+    iniciarRonda(alianzas[0], alianzas[3]);
+    iniciarRonda(alianzas[1], alianzas[2]);
+    iniciarRonda(alianzas[1], alianzas[3]);
+    iniciarRonda(alianzas[2], alianzas[3]);
+
+    cout << endl;
+    for(int i=0;i<4;i++) {
+        cout << alianzas[i].nombre << endl;
+        cout << alianzas[i].esmeraldas << endl;
+        cout << alianzas[i].puntosAFavor << endl;
+        cout << alianzas[i].puntosEnContra << endl;
+    }
 }
